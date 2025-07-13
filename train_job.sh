a093aaa0606fb7d1b5447dde81d8ed5c292982d6
Shunqi <wang2669003240@gmail.com>
2025-07-13 17:39:43 -0400

    Changed requirements.txt

--- train_job.sh
+++ train_job.sh
@@ -10,6 +10,17 @@ echo "Hello World"
 nvidia-smi
 module load python/3.12
 module load cuda
+pip uninstall -y jax jaxlib
+pip install --no-index \
+    jax==0.6.0 \
+    jaxlib==0.6.0+computecanada
+
+# 4) (Optional) verify inside the job
+python - <<EOF
+import jax
+print("JAX version:", jax.__version__)
+print("Devices:", jax.devices())
+EOF
 # Activate your enviroment
 source ~/envs/stack_t/bin/activate
 python ~/scratch/stack_Transformer/example_stack_t.py --batch_size 32 --training_steps 100000 --task reverse_string --architecture rnn --stack=FALSE --pos=NONE --seed=0
