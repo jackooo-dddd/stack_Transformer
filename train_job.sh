04daa8d96bfdbe6e8659a6dd5ee12086ec18422e
Shunqi <wang2669003240@gmail.com>
2025-07-14 21:33:30 -0400

    Redirect the print statement to standard output instead of logging.

--- train_job.sh
+++ train_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=0-01:20:00
+#SBATCH --time=0-12:20:00
 #SBATCH --account=def-vumaiha
 #SBATCH --mem=32000M
 #SBATCH --gpus-per-node=1
@@ -41,46 +41,45 @@ mkdir -p result
 # Set task folder based on user input
 TASK_DIR=~/scratch/stack_Transformer/neural_networks_chomsky_hierarchy/tasks/$SUBDIR
 TASKS=$(find "$TASK_DIR" -name '*.py' ! -name 'task.py' -exec basename {} .py \;)
-echo "Running All $SUBDIR Tasks"
+echo "*******************RUNNING ALL $SUBDIR TASKS*******************"
 for JOB_NAME in $TASKS; do
-  echo "===========Iteration For Job With Name: $JOB_NAME================"
+  echo "================================================================="
+  echo "===========ITERATION FOR JOB: $JOB_NAME=========================="
+  echo "================================================================="
   echo "---------------------------------------------------"
   echo "Start running STACK RNN for task=$JOB_NAME"
-  echo "---------------------------------------------------"
   python ~/scratch/stack_Transformer/example_stack_t.py \
       --batch_size 32 \
-      --training_steps 10 \
+      --training_steps 100000 \
       --task "$JOB_NAME" \
       --architecture stack_rnn \
       --stack=False \
       --pos=NONE \
       --seed=0
-  echo "Finish STACK RNN for task=$JOB_NAME"
+  echo "Finish running STACK RNN for task=$JOB_NAME"
 
   echo "---------------------------------------------------"
   echo "Start running STANDARD transformer_encoder for task=$JOB_NAME"
-  echo "---------------------------------------------------"
   python ~/scratch/stack_Transformer/example_stack_t.py \
       --batch_size 32 \
-      --training_steps 10 \
+      --training_steps 100000 \
       --task "$JOB_NAME" \
       --architecture transformer_encoder \
       --stack=False \
       --pos=NONE \
       --seed=0
-  echo "Finish STANDARD transformer_encoder for task=$JOB_NAME"
+  echo "Finish running STANDARD transformer_encoder for task=$JOB_NAME"
 
   echo "---------------------------------------------------"
   echo "Start running STACK AUGMENTED transformer_encoder for task=$JOB_NAME"
-  echo "---------------------------------------------------"
   python ~/scratch/stack_Transformer/example_stack_t.py \
       --batch_size 32 \
-      --training_steps 10 \
+      --training_steps 100000 \
       --task "$JOB_NAME" \
       --architecture transformer_encoder \
       --stack=True \
       --pos=NONE \
       --seed=0
-  echo "Finish STACK ATTENTION transformer_encoder for task=$JOB_NAME"
+  echo "Finish running STACK AUGMENTED transformer_encoder for task=$JOB_NAME"
 done
 
